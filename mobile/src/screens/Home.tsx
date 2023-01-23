3c2ff68dda89c9f0af01400c054d0b3f621edbab
Matheus <halkner77@hotmail.com>
2023-01-22 21:51:12 -0300

    :sparkles: feat: Adding logic with map to fill the summary table with habit day squares

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,10 +1,14 @@
-import { Text, View } from "react-native";
+import { Text, View, ScrollView } from "react-native";
 import { HabitDay } from "../components/HabitDay";
 import { Header } from "../components/Header";
 import { daySize } from "../components/HabitDay";
+import { generateRangeDatesFromYearStart } from "../utils/generate-range-between-dates";
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
 
+const datesFromYearStart = generateRangeDatesFromYearStart();
+const minimumSummaryDatesSizes = 18 * 5;
+const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
 export function Home(){
     return(
         <View className="flex-1 bg-background px-8 pt-16">
@@ -24,7 +28,33 @@ export function Home(){
                 }
             </View>
 
-            <HabitDay/>
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{ paddingBottom: 100 }}
+            >
+                <View className="flex-row flex-wrap">
+                    {
+                        datesFromYearStart.map(date => (
+                            <HabitDay
+                            key={date.toISOString()}
+                            />
+                            ))
+                        }
+
+                    {
+                        amountOfDaysToFill > 0 && Array
+                        .from({length: amountOfDaysToFill})
+                        .map((_, index) => (
+                            <View
+                            key={index}
+                            className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                            style={{ width: daySize, height: daySize }}
+                            />
+                            ))
+                        }
+                </View>
+            </ScrollView>
+
         </View>
     )
 }
