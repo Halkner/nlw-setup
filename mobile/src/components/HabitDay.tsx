3241b3de0a7a65a18deac82e34fcc6c393764e0a
Matheus <halkner77@hotmail.com>
2023-01-26 20:22:09 -0300

    Mobile Aula 4

--- mobile/src/components/HabitDay.tsx
+++ mobile/src/components/HabitDay.tsx
@@ -1,4 +1,7 @@
+import clsx from "clsx";
+import dayjs from "dayjs";
 import { Dimensions, TouchableOpacity, TouchableOpacityProps } from "react-native"
+import { generateProgressPercentage } from "../utils/generate-progress-percentage";
 
 // Cálculo para renderizar os quadrados na tela
 const weekDays = 7;
@@ -8,13 +11,29 @@ export const dayMarginBetween = 8;
 export const daySize = (Dimensions.get('screen').width / weekDays) - (screenHorizontalPadding + 5);
 
 interface Props extends TouchableOpacityProps{
-
+    amountOfHabits?: number;
+    amountCompleted?: number;
+    date: Date;
 }
 
-export function HabitDay({...rest}: Props){
+export function HabitDay({amountCompleted = 0, amountOfHabits = 0, date, ...rest}: Props){
+
+    const amountAccomplishedPercentage = amountOfHabits > 0 ? generateProgressPercentage(amountOfHabits, amountCompleted) : 0
+
+    const today = dayjs().startOf('day').toDate();
+    const isCurrentDay = dayjs(date).isSame(today);
+
     return(
         <TouchableOpacity
-            className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800"
+            className={clsx("rounded-lg border-2 m-1", {
+                ["bg-zinc-900 border-zinc-800"] : amountAccomplishedPercentage === 0,
+                ["bg-violet-900 border-violet-700"] : amountAccomplishedPercentage > 0 && amountAccomplishedPercentage < 20,
+                ["bg-violet-800 border-violet-600"] : amountAccomplishedPercentage > 20 && amountAccomplishedPercentage < 40,
+                ["bg-violet-700 border-violet-500"] : amountAccomplishedPercentage > 40 && amountAccomplishedPercentage < 60,
+                ["bg-violet-600 border-violet-500"] : amountAccomplishedPercentage > 60 && amountAccomplishedPercentage < 80,
+                ["bg-violet-500 border-violet-400"] : amountAccomplishedPercentage > 80,
+                ["border-white border-4"] : isCurrentDay
+            })}
             style={{ width: daySize, height: daySize }}
             activeOpacity={0.7}
             {...rest}
